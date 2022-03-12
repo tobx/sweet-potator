28aa47d09b365f62db5610022dba7e562f27ce5b
tobx <net@tobx.de>
2022-03-12 19:19:22 +0100

    Add history API with url hashes for tags

--- src/templates/html/static/js/index.js
+++ src/templates/html/static/js/index.js
@@ -12,11 +12,14 @@
       this.elements = [];
     }
 
-    toggle(force) {
-      this.isActive = force === undefined ? !this.isActive : force;
+    refresh() {
       for (const element of this.elements) {
         element.classList.toggle("active", this.isActive);
       }
+    }
+
+    toggle(force) {
+      this.isActive = force === undefined ? !this.isActive : force;
     }
   }
 
@@ -45,22 +48,24 @@
     }
 
     addEventListeners() {
+      window.addEventListener("hashchange", () => this.updateFromUrlHash());
       for (const tag of this.tags.values()) {
         for (const element of tag.elements) {
           element.addEventListener("click", () => {
             tag.toggle();
-            this.refresh();
+            this.updateUrlHash();
           });
         }
       }
       for (const element of document.querySelectorAll(selectors.tagReset)) {
-        element.addEventListener("click", () => {
-          this.reset();
-        });
+        element.addEventListener("click", () => this.reset());
       }
     }
 
     refresh() {
+      for (const tag of this.tags.values()) {
+        tag.refresh();
+      }
       for (const tagged of this.tagged) {
         const show = Array.from(this.tags)
           .filter(([, tag]) => tag.isActive)
@@ -73,7 +78,84 @@
       for (const tag of this.tags.values()) {
         tag.toggle(false);
       }
+      this.updateUrlHash();
+    }
+
+    updateUrlHash() {
+      const params = new URLSearchParams(location.hash.slice(1));
+      if ([...this.tags.values()].some((tag) => tag.isActive)) {
+        params.set(
+          "tags",
+          this.constructor.encodeTagNames(
+            [...this.tags]
+              .filter(([, tag]) => tag.isActive)
+              .map(([name]) => name)
+          )
+        );
+      } else {
+        params.delete("tags");
+      }
+      // not using `params.toString()` to have pretty urls (browser already takes care of url encoding)
+      const hash = [...params].map((entry) => entry.join("=")).join("&");
+      let path = location.pathname;
+      const isRecipePage = document.querySelector("main > .recipe") !== null;
+      if (isRecipePage) {
+        let index = path.lastIndexOf("/");
+        index = path.lastIndexOf("/", index - 1);
+        path = path.slice(0, index + 1);
+      }
+      if (hash !== "") {
+        path += "#" + hash;
+      }
+      history.pushState("", "", path);
+      if (isRecipePage) {
+        location.reload();
+      } else {
+        this.updateFromUrlHash();
+      }
+    }
+
+    updateFromUrlHash() {
+      const params = new URLSearchParams(location.hash.slice(1));
+      const tagNames = this.constructor.decodeTagNames(
+        params.get("tags") ?? ""
+      );
+      for (const [name, tag] of this.tags) {
+        const activate = tagNames.has(name);
+        if (activate !== tag.isActive) {
+          tag.toggle(activate);
+        }
+      }
       this.refresh();
+    }
+
+    static encodeTagNames(tags) {
+      return tags
+        .map((text) => text.replace("\\", "\\\\").replace(",", "\\,"))
+        .join(",");
+    }
+
+    static decodeTagNames(text) {
+      const tags = new Set();
+      let escape = false;
+      let current = "";
+      for (const c of text) {
+        if (escape) {
+          current += c;
+          escape = false;
+        } else if (c === "\\") {
+          escape = true;
+        } else if (c === ",") {
+          tags.add(current);
+          current = "";
+        } else {
+          current += c;
+        }
+      }
+      if (current !== "") {
+        tags.add(current);
+      }
+      return tags;
     }
   }
 
@@ -95,14 +177,14 @@
     for (const [name, element] of queryTagElements(document)) {
       tags.addTagElement(name, element);
     }
-    for (const element of document.querySelectorAll(selectors.favorites)) {
-      tags.addTagElement("favorite", element);
-    }
+    const favoritesElement = document.querySelector(selectors.favorites);
+    tags.addTagElement(favoritesElement.dataset.tagName, favoritesElement);
     const recipes = document.querySelectorAll(selectors.recipe);
     for (const element of recipes) {
       tags.addTaggedElement(element);
     }
     tags.addEventListeners();
+    tags.updateFromUrlHash();
   }
 
   window.addEventListener("DOMContentLoaded", initialize);
