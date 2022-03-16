58661796b3c889a35e318c522b2f305ad69a5fe5
tobx <code@tobx.de>
2022-03-16 05:40:32 +0100

    Align gh-pages deploy script with `docs` folder

--- gh-pages/deploy.sh
+++ gh-pages/deploy.sh
@@ -5,19 +5,21 @@ set -e
 self_dir="$(realpath -- "$(dirname -- "$0")")"
 project_dir="$(dirname -- "${self_dir}")"
 
+verify_branch() {
+    current_branch=$(git branch --show-current)
+    if [ "$current_branch" != "$1" ]; then
+        >&2 echo "Error: current branch is not '${1}' but '${current_branch}'"
+        return 1
+    fi
+    if [ -n "$(git status --porcelain)" ]; then
+        >&2 echo "Error: there are uncommitted changes"
+        return 1
+    fi
+}
+
 cd "${project_dir}"
 
-current_branch=$(git branch --show-current)
-
-if [ "$current_branch" != "main" ]; then
-    >&2 echo "Error: current branch is not 'main' but '${current_branch}'"
-    exit 1
-fi
-
-if [ -n "$(git status --porcelain)" ]; then
-    >&2 echo "Error: there are uncommitted changes"
-    exit 1
-fi
+verify_branch "main"
 
 printf "Running tests: "
 if cargo test --quiet --release --all-features &> /dev/null; then
@@ -30,7 +32,7 @@ fi
 
 cargo build --release
 
-config_dir="${self_dir}/sweet-potator"
+config_dir="${self_dir}/config"
 recipe_dir="${self_dir}/recipes"
 output_dir="${self_dir}/dist"
 
@@ -41,15 +43,16 @@ rm -rf "${config_dir}" "${output_dir}"
     --recipe-dir "${recipe_dir}" \
     build "${output_dir}"
 
+rm -rf "${config_dir}"
+
 datetime=$(date -u +%Y-%m-%dT%TZ)
 commit=$(git rev-parse --short HEAD)
 git checkout "gh-pages"
-git rm -rf .
-echo "checkout '.gitignore'"
-git checkout HEAD -- ".gitignore"
-echo "copy from '${self_dir}/dist'"
-cp -R "${self_dir}/dist/." .
-git add .
+verify_branch "gh-pages"
+git rm -rf "docs"
+echo "move '${self_dir}/dist' to '${project_dir}/docs'"
+mv "${self_dir}/dist" "${project_dir}/docs"
+git add "docs"
 if [ -n "$(git status --porcelain)" ]; then
     git commit -m "Deploy main@${commit} (${datetime})"
 else
